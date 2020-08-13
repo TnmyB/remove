cc3284d064a432b4b90183ca433ae21a96088a60
TnmyB <68606780+TnmyB@users.noreply.github.com>
2020-08-13 15:28:49 +0530

    Update no_oggy.cs

--- no_oggy.cs
+++ no_oggy.cs
@@ -5,13 +5,25 @@ class NoOggyChecker
 {
   static void RemoveOggy(List<string> names)
   {
-    foreach (var name in names)
+    //Initial method in comment
+   /* foreach (var name in names)
     {
         if(name.StartsWith("oggy")) {
             Console.WriteLine($"Need to remove {name}!");
             names.Remove(name);
         }
     }
+    */
+    
+    for(int i_names = 0; i_names < names.Count ; i_names++){
+           
+           if(names[i_names].StartsWith("oggy")){
+                 Console.WriteLine($"Need to remove {names[i_names]}!");
+                 names.Remove(names[i_names]);
+                 i_names--;
+           }
+    }
+    
   }
   static bool OggyIsRemoved(List<string> names)
   {
